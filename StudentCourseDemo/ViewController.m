fd25dad8bd9bb01122e5ce1cbf9061f8809e93fb
lingtingSir <363905547@qq.com>
2015-11-02 22:35:42 +0800

    Initial Commit

---
+++ StudentCourseDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StudentCourseDemo
+//
+//  Created by lmj on 15/11/2.
+//  Copyright (c) 2015年 lmj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
